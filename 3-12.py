5265673d9f5899f4417110f670828a54bb54e8e1
xiang3121 <chuliyang@163.com>
2019-06-20 15:39:28 +0800

    tijiao

---
+++ 3-12.py
@@ -0,0 +1,36 @@
+
+# *_*coding:utf-8 *_*
+
+print "你是猪"
+print "你是\n猪"
+print "\t你\n是\n\t猪"
+
+print "\n你\是\猪"
+
+print "hello world"
+print "hello again"
+print "i like typing this."
+print "this is fun."
+print 'yay! printing.'
+print "I'd much rather you 'not'."
+print 'I"said" do not touch this.'
+print "I will now count my chickens:"
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 *3%4
+print "Now I will  count  the eggs:"
+print  3 + 2 + 1 -5 + 4 % 2 -1 / 4 +6
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+print "Oh, that's why it's False."
+print "How about some more."
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+cars =100
+space_in_a_car =4.0
+print "There are", cars, "cars available."
+print "There are", cars,"cars available"
+
+
